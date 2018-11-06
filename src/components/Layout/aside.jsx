d844f8b27c4ce5f52a2f5ea74e1bcac843f54573
Evgeny Leukhin <eugenel@effective.band>
2018-11-06 16:30:00 +0600

    Add aside toggle

--- src/components/Layout/aside.jsx
+++ src/components/Layout/aside.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+// import CSSTransition from 'react-transition-group/CSSTransition';
+import cx from 'classnames';
 
-const styles = {
-  fontWeight: 'bold'
+export default props => {
+  const styles = { fontWeight: 'bold' }
+  return(
+    <aside className={cx({'hide': props.hideAside })}>
+      <ul className='content-list'>
+        <li><NavLink to='/page-2' activeStyle={styles}>page-2</NavLink></li>
+        <li><NavLink to='/page-3' activeStyle={styles}>page-3</NavLink></li>
+      </ul>
+      <i onClick={props.toggleAside}></i>
+    </aside>
+  )
 }
-
-export default () => (
-  <aside>
-    <ul className="content-list">
-      <li><NavLink to='/page-2' activeStyle={styles}>page-2</NavLink></li>
-      <li><NavLink to='/page-3' activeStyle={styles}>page-3</NavLink></li>
-    </ul>
-  </aside>
-)
--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -6,14 +6,18 @@ import Routes from './../Routes'
 
 export default class Layout extends React.Component {
   state = {
-
+    showAside: true
   }
+  toggleSidebar = () => this.setState({showAside: !this.state.showAside})
 
   render() {
     return(
       <div className="page-wrapper">
         <Header/>
-        <Aside/>
+        <Aside
+          hideAside={!this.state.showAside}
+          toggleAside={this.toggleSidebar}
+        />
         <Routes/>
       </div>
     )
