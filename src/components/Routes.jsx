b4fe2d9a02f82b419d729b212c36e8bb2c4127a2
Evgeny Leukhin <eugeny.leukhin@stellarsolvers.com>
2019-08-15 05:05:13 +0600

    Add pages

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -4,6 +4,9 @@ import {
   // main pages
   ReacT, Redux, JS, Webpack, NodeJs,
 
+  // js pages
+  JsScript, JsTypes, JsVars, JsString, JsNumber, JsBoolean, JsFunction, JsArray, JsObject, JsOperators, JsLogic,JsCycles,
+
   // webpack-pages
   WpInstall, WpConfig, WpJs, WpCss, WpHtml, WpImg, WpFonts, WpMd, WpPlugins, WpEslint, WpEditor, WpStructure,
 
@@ -20,14 +23,43 @@ import Test from 'Components/Test';
 const Routes = () => (
   <main className='markdown-body'>
     <Switch>
-      <Route exact path="/" render={() => <Redirect to="/react" /> } />
+      <Route exact path="/" render={() => <Redirect to="/js" /> } />
 
       {/* main pages */}
-      <Route path='/react'  exact component={ReacT} />
       <Route path='/js'     exact component={JS} />
-      <Route path='/redux'  exact component={Redux} />
       <Route path='/wp'     exact component={Webpack} />
+      <Route path='/react'  exact component={ReacT} />
+      <Route path='/redux'  exact component={Redux} />
       <Route path='/nodejs' exact component={NodeJs} />
+      <Route path='/test' component={Test} />
+
+      {/* js pages */}
+      <Route path='/js/script'      component={JsScript} />
+      <Route path='/js/types'       component={JsTypes} />
+      <Route path='/js/vars'        component={JsVars} />
+      <Route path='/js/string'      component={JsString} />
+      <Route path='/js/number'      component={JsNumber} />
+      <Route path='/js/boolean'     component={JsBoolean} />
+      <Route path='/js/function'    component={JsFunction} />
+      <Route path='/js/array'       component={JsArray} />
+      <Route path='/js/object'      component={JsObject} />
+      <Route path='/js/operators'   component={JsOperators} />
+      <Route path='/js/logic'       component={JsLogic} />
+      <Route path='/js/cycles'      component={JsCycles} />
+
+      {/* wp-pages */}
+      <Route path='/wp/install'   component={WpInstall} />
+      <Route path='/wp/config'    component={WpConfig} />
+      <Route path='/wp/js'        component={WpJs} />
+      <Route path='/wp/css'       component={WpCss} />
+      <Route path='/wp/img'       component={WpImg} />
+      <Route path='/wp/fonts'     component={WpFonts} />
+      <Route path='/wp/md'        component={WpMd} />
+      <Route path='/wp/html'      component={WpHtml} />
+      <Route path='/wp/plugins'   component={WpPlugins} />
+      <Route path='/wp/eslint'    component={WpEslint} />
+      <Route path='/wp/editor'    component={WpEditor} />
+      <Route path='/wp/structure' component={WpStructure} />
 
       {/* react-pages */}
       <Route path='/react/template'    component={ReactTemplate} />
@@ -53,28 +85,11 @@ const Routes = () => (
       <Route path='/react/plugins'     component={ReactPlugins} />
       <Route path='/react/gatsby-js'   component={ReactGatsby} />
 
-      {/* wp-pages */}
-      <Route path='/wp/install'   component={WpInstall} />
-      <Route path='/wp/config'    component={WpConfig} />
-      <Route path='/wp/js'        component={WpJs} />
-      <Route path='/wp/css'       component={WpCss} />
-      <Route path='/wp/img'       component={WpImg} />
-      <Route path='/wp/fonts'     component={WpFonts} />
-      <Route path='/wp/md'        component={WpMd} />
-      <Route path='/wp/html'      component={WpHtml} />
-      <Route path='/wp/plugins'   component={WpPlugins} />
-      <Route path='/wp/eslint'    component={WpEslint} />
-      <Route path='/wp/editor'    component={WpEditor} />
-      <Route path='/wp/structure' component={WpStructure} />
-
       {/* nodejs-pages */}
       <Route path='/nodejs/express' component={NodeJsExpress} />
 
-      <Route path='/test' component={Test} />
-
       {/* 404 page */}
       <Route render={() => <h1 style={{ paddingTop: 24 }}>404 - Page not found</h1>} />
-
     </Switch>
   </main>
 );
